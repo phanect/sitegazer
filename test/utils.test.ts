b89404c529bbfd9d51b101eacc3a644538401692
Jumpei Ogawa <phanective@gmail.com>
2019-08-04 04:02:00 +0900

    test: add test for utils.js#deduplicate()

---
+++ test/utils.test.ts
@@ -0,0 +1,31 @@
+"use strict";
+
+import { deduplicate } from "../src/utils";
+
+test("deduplicate", async () => {
+  const deduplicated = deduplicate([
+    "www.example.com",
+    "www.example.org",
+    "www.example.net",
+    "www.example.io",
+
+    "www.example.net",
+    "www.example.com",
+    "www.example.net",
+    "www.example.net",
+    "www.example.net",
+    "www.example.org",
+    "www.example.io",
+    "www.example.org",
+    "www.example.net",
+    "www.example.org",
+    "www.example.io",
+  ]);
+
+  expect(deduplicated).toEqual([
+    "www.example.com",
+    "www.example.org",
+    "www.example.net",
+    "www.example.io",
+  ]);
+});
