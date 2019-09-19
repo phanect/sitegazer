c01ad022d17a00e20128bd12d2c34c1bd8f2d5f2
Jumpei Ogawa <phanective@gmail.com>
2019-09-19 20:50:53 +0900

    test: add test to check if error returned when the host doesn't respond

--- test/sitelint.test.ts
+++ test/sitelint.test.ts
@@ -1,6 +1,7 @@
 import SiteLint from "../src/sitelint";
 
 const url = "https://localhost:3456";
+const inexistentURL = "http://localhost:7171";
 
 test("SiteLint crawls the URLs in the pages", async () => {
   const sitelint = new SiteLint({
@@ -20,4 +21,26 @@ test("SiteLint crawls the URLs in the pages", async () => {
     `${url}/link1.html`,
     `${url}/link2.html`,
   ]);
+});
+
+test("SiteLint returns an error if specified host doesn't respond.", async () => {
+  const sitelint = new SiteLint({
+    urls: [
+      inexistentURL,
+    ],
+    sitemap: false,
+    crawl: true,
+    plugins: [],
+  });
+  const results = await sitelint.run();
+
+  expect(results).toEqual([{
+    url: "http://localhost:7171",
+    pluginName: null,
+    errors: [{
+      message: "Error: Request failure for http://localhost:7171. Server doesn't respond.",
+      line: 1,
+      column: 1,
+    }],
+  }]);
 });
