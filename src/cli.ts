64b0f76644bed4da848fc9a7a8e7613bf784ac98
Jumpei Ogawa <phanective@gmail.com>
2019-09-26 00:23:57 +0900

    refactor: rewrite without commander.action()

--- src/cli.ts
+++ src/cli.ts
@@ -5,25 +5,26 @@ import SiteLint from "./sitelint";
 commander
   .version("0.1.0")
   .arguments("<url>")
-  .option("-c, --config <config>", "Path to configuration file")
-  .action((url, opts) => {
-    (async () => {
-      const config = require(opts.config ?
-        opts.config :
-        join(process.cwd(), ".sitelintrc.js")
-      );
+  .option("-c, --config <config>", "Path to configuration file");
 
-      config.urls.push(url);
+commander.parse(process.argv);
 
-      const sitelint = new SiteLint(config);
-      const warnings = await sitelint.run();
+(async () => {
+  const config = require(commander.config ?
+    commander.config :
+    join(process.cwd(), ".sitelintrc.js")
+  );
 
-      for (const warning of warnings) {
-        console.info(warning.url);
-        console.info(warning.pluginName);
-        console.info(warning.message);
-        console.info(warning.line);
-        console.info(warning.column);
-      }
-    })();
-  }).parse(process.argv);
+  config.urls.push(commander.url);
+
+  const sitelint = new SiteLint(config);
+  const warnings = await sitelint.run();
+
+  for (const warning of warnings) {
+    console.info(warning.url);
+    console.info(warning.pluginName);
+    console.info(warning.message);
+    console.info(warning.line);
+    console.info(warning.column);
+  }
+})();
