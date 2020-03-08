2a7f56c381ba191074ba5714abbeb5571c1850a2
Jumpei Ogawa <phanective@gmail.com>
2020-03-08 19:14:32 +0900

    refactor: expand onError() function to anonymous callback functions
    to run different process for "error" event and "pageerror" event.

--- src/sitegazer.ts
+++ src/sitegazer.ts
@@ -70,7 +70,13 @@ class SiteGazer {
 
   private async loadPage(url: string, deviceType: string, userAgent: string): Promise<void> {
     const issues: Issue[] = [];
-    const onError = (err: Error): void => {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    page.setUserAgent(userAgent);
+
+    page.on("error", err => {
       issues.push({
         pageURL: url,
         fileURL: url,
@@ -80,14 +86,17 @@ class SiteGazer {
         line: 0,
         column: 0,
       });
-    };
-
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    page.setUserAgent(userAgent);
-    page.on("pageerror", onError);
-    page.on("error", onError);
+    }).on("pageerror", err => {
+      issues.push({
+        pageURL: url,
+        fileURL: url,
+        deviceType,
+        pluginName: "Chrome Console",
+        message: err.toString(),
+        line: 0,
+        column: 0,
+      });
+    });
 
     try {
       const res = await page.goto(url);
